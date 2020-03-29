e8c2a8b24b870d3b6dee69db0e07a5a7c05c9e20
sarababaei <sarababaei212@gmail.com>
2020-03-30 03:10:56 +0430

    joining lobby with server

--- KhatKhati/JoinGameViewController.swift
+++ KhatKhati/JoinGameViewController.swift
@@ -9,12 +9,62 @@
 import Foundation
 import UIKit
 
-class JoinGameViewController: UIViewController {
-    
+class JoinGameViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Defenitions
     @IBOutlet weak var randomGameButton: CustomButton!
     @IBOutlet weak var lobbyCodeTextField: UITextField!
     @IBOutlet weak var joinButton: CustomButton!
     
+    @IBAction func randomGameAction(_ sender: Any) {
+        print("^^^^^^^^")
+        SocketIOManager.sharedInstance.getPlayer()
+        print("^^^^^^^^")
+        
+    }
+    //MARK: Keyboard management
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification){
+        let keyboardFrame = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        adjustLayoutForKeyboard(targetHight: keyboardFrame.size.height)
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        adjustLayoutForKeyboard(targetHight: 0)
+    }
+    
+    func adjustLayoutForKeyboard(targetHight: CGFloat){
+//        tableView.contentInset.bottom = targetHight
+    }
+    
+    @IBAction func joinGameAction(_ sender: Any) {
+        if let caption = fetchInput() {
+            SocketIOManager.sharedInstance.joinGame(roomID: caption, username: "Player0")
+            lobbyCodeTextField.resignFirstResponder()
+        }
+    }
+    
+    func manageJoinButton(){
+        var enabled = false
+        if let caption = fetchInput() {
+            if caption.count > 0 {  //useless?!
+                enabled = true
+            }
+        }
+        joinButton.isEnabled = enabled
+    }
+    
+    func fetchInput() -> String? {
+        if let caption = lobbyCodeTextField.text?.trimmingCharacters(in: .whitespaces) {
+            return caption.count > 0 ? caption : nil
+        }
+        return nil
+    }
+    
+    //MARK: UI handling
     func setRandomGameButtonAttributes() {
         randomGameButton.setCornerRadius(radius: 30)
         randomGameButton.setShadowColor(color: Colors.blue.componentColor!)
@@ -36,6 +86,8 @@ class JoinGameViewController: UIViewController {
         setRandomGameButtonAttributes()
         setLobbyCodeTextFieldAttributes()
         setJoinButtonAttributrs()
+        
+        lobbyCodeTextField.delegate = self
     }
     
     override func viewDidLoad() {
